64c04880a6b0f801a4d935ed248fb727a65b3d72
Nick Ada <68816988+nick13ada@users.noreply.github.com>
2024-07-11 20:19:07 +0200

    Fix LocalizeString func in ArticyLocalizerSystem.h returning BackupText before checking for ResolveTextExtension

--- Source/ArticyRuntime/Public/ArticyLocalizerSystem.h
+++ Source/ArticyRuntime/Public/ArticyLocalizerSystem.h
@@ -99,16 +99,17 @@ public:
 			return FText(SourceString);
 		}
 
-		if (BackupText)
-		{
-			return *BackupText;
-		}
-
 		// By default, return via the key
 		if (ResolveTextExtension)
 		{
 			return ResolveText(Outer, &Key);
 		}
+
+		if (BackupText)
+		{
+			return *BackupText;
+		}
+		
 		return Key;
 	}
 
